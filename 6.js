3a6c451beb618f868e26a4ad546964ffd9e7ba6c
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2022-07-06 00:09:44 -0400

    Day 6

---
+++ 6.js
@@ -0,0 +1,29 @@
+
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+// An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+// Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+// Input: nums = [1,2,2,3]
+// Output: true
+
+nums = [1,2,2,3]
+var isMonotonic = function(nums) {
+    let increasing = true 
+    let decreasing = true
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        if(nums[i] > nums[i+1]){
+            increasing = false 
+        }
+        
+        if(nums[i] < nums[i+1]){
+            decreasing = false
+        }
+        
+    }
+    
+    return increasing || decreasing;
+};
+console.log(isMonotonic(nums))
