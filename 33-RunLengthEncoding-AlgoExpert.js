b37204a550d7f6bf275dd78bd423eb549ed245b8
Hemanth Thallapaneni <tallapaneni.hemanth@gmail.com>
2022-08-08 12:13:06 -0500

    Day 28

---
+++ 33-RunLengthEncoding-AlgoExpert.js
@@ -0,0 +1,34 @@
+// Run-Length Encoding
+// Write a function that takes in a non-empty string and returns its run-length encoding.
+
+// From Wikipedia, "run-length encoding is a form of lossless data compression in which runs of data are stored as a single data value and count, rather than as the original run." For this problem, a run of data is any sequence of consecutive, identical characters. So the run "AAA" would be run-length-encoded as "3A".
+
+// To make things more complicated, however, the input string can contain all sorts of special characters, including numbers. And since encoded data must be decodable, this means that we can't naively run-length-encode long runs. For example, the run "AAAAAAAAAAAA" (12 As), can't naively be encoded as "12A", since this string can be decoded as either "AAAAAAAAAAAA" or "1AA". Thus, long runs (runs of 10 or more characters) should be encoded in a split fashion; the aforementioned run should be encoded as "9A3A".
+
+// Sample Input
+// string = "AAAAAAAAAAAAABBCCCCDD"
+// Sample Output
+// "9A4A2B4C2D"
+
+
+//Time complexity: O(n) | space - O(n)
+string = "aA"
+
+function runLengthEncoding(string) {
+  let count = 1;
+  let encodedoutput = [];
+
+for(let i=0;i<string.length;i++){
+    if(string[i] === string[i+1] && count < 9){
+            count++; 
+    }
+    else{
+        encodedoutput.push(`${count}${string[i]}`);
+        count =1;
+    }
+   
+}
+return encodedoutput.join('')
+}
+
+console.log(runLengthEncoding(string));
